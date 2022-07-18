84f290886dbaf351c37d6a0589281b81ead3a321
Andrew Faugno <andrewfaugno825@gmail.com>
2022-07-18 19:44:52 -0400

    created fetch request on frontend to create pizzas

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -51,7 +51,7 @@ const pizzaController = {
 
     // delete a pizza
     deletePizza({ params }, res) {
-        Pizza.findOneAndDelete({ _id: params,id })
+        Pizza.findOneAndDelete({ _id: params.id })
             .then(dbPizzaData => {
                 if (!dbPizzaData) {
                     res.status(404).json({ message: 'No pizza found with this id!' });
